25c0a76b44ef6941f53997b37ace59bcb2154001
hituji_code <k.m.hituji3340@gmail.com>
2024-04-05 16:59:28 +0900

    ヘッダー途中

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,47 @@
+jQuery(function ($) {
+  // この中であればWordpressでも「$」が使用可能になる
 
-jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+  // ハンバーガーメニュー
+  let scrollPosition = 0;
 
+  function openDrawer() {
+    scrollPosition = $(window).scrollTop();
+    $(".js-header .header__inner").addClass("is-open");
+    $(".js-drawer").addClass("is-open");
+    $(".js-hamburger").addClass("is-open");
+    $("body").addClass("is-fixed").css("top", -scrollPosition);
+  }
 
+  function closeDrawer() {
+    $(".js-header .header__inner").removeClass("is-open");
+    $(".js-drawer").removeClass("is-open");
+    $(".js-hamburger").removeClass("is-open");
+    $("body").removeClass("is-fixed").css("top", "");
+    $(window).scrollTop(scrollPosition);
+  }
+
+  $(".js-hamburger").click(function () {
+    $(this).toggleClass("is-open");
+    if ($(this).hasClass("is-open")) {
+      openDrawer();
+    } else {
+      closeDrawer();
+    }
+  });
+
+  $(".js-drawer, .js-drawer a[href]").on("click", function () {
+    closeDrawer();
+  });
+
+  $(window).on("resize", function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      closeDrawer();
+    }
+  });
+
+  // Q&Aアコーディオン
+  $(".js-faq").on("click", function () {
+    $(this).next().slideToggle();
+    $(this).toggleClass("is-open");
+  });
 });
